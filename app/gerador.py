06379e3a37fe3d713e0c02f088fc1f28c09a83db
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2025-05-19 14:52:44 -0300

    Update gerador.py

--- app/gerador.py
+++ app/gerador.py
@@ -42,7 +42,7 @@ class GeradorAssuntoODS:
         ]
 
         model = genai.GenerativeModel(
-            model_name='models/gemini-1.5-pro',
+            model_name='models/gemini-2.0-flash',
             generation_config=generation_config,
             safety_settings=safety_settings
         )
