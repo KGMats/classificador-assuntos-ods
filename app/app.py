dca3c16ec3d83109e6347f8a7feed026d119d55f
KGMats <72230429+KGMats@users.noreply.github.com>
2025-10-29 12:07:05 +0000

    Corrigida falha de geração da lista de ODS após atualização.

--- app/app.py
+++ app/app.py
@@ -1,22 +1,22 @@
-#PROCESSADOR PDF - Subject and SDG Classifier powered by Generative AI
+# PROCESSADOR PDF - Subject and SDG Classifier powered by Generative AI
 import tempfile
 import streamlit as st
 
 from gerador import GeradorAssuntoODS
 
 
-#CONFIGURAÇÕES INICIAIS
+# CONFIGURAÇÕES INICIAIS
 st.set_page_config(
     layout='wide',
     page_title='ia-geratemas',
     page_icon='::robot_face::'
 )
 
-#TÍTULO DO APP
-st.title('IA GeraTemas',anchor='string')
-st.header('Gerador de Assuntos e ODS',anchor=False)
+# TÍTULO DO APP
+st.title('IA GeraTemas', anchor='string')
+st.header('Gerador de Assuntos e ODS', anchor=False)
 
-#PREÂMBULO
+# PREÂMBULO
 st.write('''
         Bem-vindo ao IA GeraTemas, uma aplicação que utiliza inteligência artificial generativa (Gemini - Google) para identificar e gerar os principais assuntos e Objetivos de Desenvolvimento Sustentável (ODS) de documentos. A ferramenta é projetada para facilitar a análise de textos, proporcionando uma compreensão rápida e precisa dos temas abordados.
         ''')
@@ -37,26 +37,26 @@ st.markdown('&nbsp;')
 st.divider()
 st.markdown("&nbsp;")
 
-#INPUT DE DADOS
+# INPUT DE DADOS
 api_key = st.text_input('Insira sua API Key do Gemini', 'AIpsS0AfgGsD3Pd3g_dfrvUsdL78L7u8LM_md-cg')
-uploaded_file = st.file_uploader('Faça o upload do arquivo:', type=['pdf'],accept_multiple_files=False)
+uploaded_file = st.file_uploader('Faça o upload do arquivo:', type=['pdf'], accept_multiple_files=False)
 
 
-#INTERFACE
+# INTERFACE
 if st.button('Processar'):
     if uploaded_file is not None:
         with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
             temp_file.write(uploaded_file.read())
             temp_pdf_path = temp_file.name
-            
+
             try:
                 gerador = GeradorAssuntoODS(api_key=api_key, caminho_pdf=temp_pdf_path)
                 assuntos = gerador.catalogar_assunto()
                 ods = gerador.catalogar_ODS()
-                
+
                 st.write(f'ASSUNTOS:\n\n{assuntos}')
                 st.write(f'ODS:\n\n{ods}')
-                
+
             except Exception as e:
                 st.error(f'Houve um problema na API. Por favor, recarregue a página e tente novamente. Detalhes do erro: {str(e)}')
     else:
@@ -68,4 +68,3 @@ st.markdown('''
     Elaborado por Francisco Tadeu Gonçalves de Oliveira Foz.\n
     GitHub: [FranciscoFoz](https://github.com/FranciscoFoz)
     ''')
-
--- app/gerador.py
+++ app/gerador.py
@@ -1,37 +1,16 @@
 # GERADOR
-
-import PyPDF2
-import tempfile
 from google import genai
 from google.genai import types
+
 
 class GeradorAssuntoODS:
     def __init__(self, api_key, caminho_pdf):
         self.client = self.configurar_client(api_key)
         self.caminho_pdf = caminho_pdf
 
-
     def configurar_client(self, api_key):
         client = genai.Client(api_key=api_key)
         return client
-
-    def transformar_pdf_texto(self):
-        arquivo_pdf = open(self.caminho_pdf, 'rb')
-        leitor_pdf = PyPDF2.PdfReader(arquivo_pdf)
-        texto = ''
-
-        for pagina_num in range(len(leitor_pdf.pages)):
-            page = leitor_pdf.pages[pagina_num]
-            texto += page.extract_text()
-            texto += '\n\n'
-
-        arquivo_pdf.close()
-
-        with tempfile.NamedTemporaryFile(delete=False, suffix='.txt', mode='w', encoding='utf-8') as arquivo_saida:
-            arquivo_saida.write(texto)
-            caminho_arquivo_txt = arquivo_saida.name
-
-        return caminho_arquivo_txt
 
     def carregar_arquivo_genai(self, caminho_arquivo):
         arquivo = self.client.files.upload(
@@ -43,11 +22,8 @@ class GeradorAssuntoODS:
         return arquivo
 
     def processar_arquivo(self):
-        caminho_txt = self.transformar_pdf_texto()
-        arquivo = self.carregar_arquivo_genai(caminho_txt)
-
+        arquivo = self.carregar_arquivo_genai(self.caminho_pdf)
         return arquivo
-
 
     def get_safety_settings(self):
         return [
@@ -68,10 +44,9 @@ class GeradorAssuntoODS:
                 'threshold': 'BLOCK_NONE'
             },
         ]
+
     def catalogar_assunto(self):
         arquivo = self.processar_arquivo()
-        
-        
 
         prompt = '''
         Catalogue os assuntos desse documento conforme o campo 653$a do MARC 21.
@@ -80,8 +55,7 @@ class GeradorAssuntoODS:
         - Responda apenas os assuntos.
         - Liste os assuntos.
         '''
-        
-        
+
         response = self.client.models.generate_content(
             model='models/gemini-2.0-flash',
             contents=[prompt, arquivo],
@@ -93,13 +67,13 @@ class GeradorAssuntoODS:
                 top_k=0
             ),
         )
-        
+
         return response.text
 
     def catalogar_ODS(self):
-      
+
         arquivo = self.processar_arquivo()
-        
+
         prompt = '''
         Catalogue a ODS (Objetivos do Desenvolvimento Sustentável) que esse documento representa.
 
@@ -107,67 +81,67 @@ class GeradorAssuntoODS:
 
         Seguem ODS's:
         """
-        ODS 1. Erradicação da pobreza: 
+        ODS 1. Erradicação da pobreza:
         Acabar com a pobreza em todas as suas formas, em todos os lugares.
-        
+
         ODS 2. Erradicação da fome:
-        Acabar com a fome, alcançar a segurança alimentar e melhoria da nutrição e promover a agricultura sustentável.     
-        
+        Acabar com a fome, alcançar a segurança alimentar e melhoria da nutrição e promover a agricultura sustentável.   
+
         ODS 3. Saúde e bem-estar:
         Assegurar uma vida saudável e promover o bem-estar para todas e todos, em todas as idades.
-        
+
         ODS 4. Educação de qualidade:
         Objetivo 4. Assegurar a educação inclusiva e equitativa e de qualidade, e promover oportunidades de aprendizagem ao longo da vida para todas e todos.
-        
+
         ODS 5. Igualdade de gênero:
         Objetivo 5. Alcançar a igualdade de gênero e empoderar todas as mulheres e meninas.
-        
+
         ODS 6. Água potável e saneamento:
         Objetivo 6. Assegurar a disponibilidade e gestão sustentável da água e saneamento para todas e todos.
-        
+
         ODS 7. Energia acessível e limpa:
         Objetivo 7. Assegurar o acesso confiável, sustentável, moderno e a preço acessível à energia para todas e todos.
-        
+
         ODS 8. Trabalho decente e crescimento econômico:
         Objetivo 8. Promover o crescimento econômico sustentado, inclusivo e sustentável, emprego pleno e produtivo e trabalho decente para todas e todos.
-        
+
         ODS 9. Inovação e infraestrutura:
         Objetivo 9. Construir infraestruturas resilientes, promover a industrialização inclusiva e sustentável e fomentar a inovação.
-        
+
         ODS 10. Redução das desigualdades:
         Objetivo 10. Reduzir a desigualdade dentro dos países e entre eles.
-        
+
         ODS 11. Cidades e comunidades sustentáveis:
         Objetivo 11. Tornar as cidades e os assentamentos humanos inclusivos, seguros, resilientes e sustentáveis.
-        
+
         ODS 12. Consumo e produção responsáveis:
         Objetivo 12. Assegurar padrões de produção e de consumo sustentáveis
-        
+
         ODS 13. Ação contra a mudança global do clima:
         Objetivo 13. Tomar medidas urgentes para combater a mudança climática e seus impactos.
-        
+
         ODS 14. Vida na água:
         Objetivo 14. Conservação e uso sustentável dos oceanos, dos mares e dos recursos marinhos para o desenvolvimento sustentável.
-        
+
         ODS 15. Vida terrestre:
         Objetivo 15. Proteger, recuperar e promover o uso sustentável dos ecossistemas terrestres, gerir de forma sustentável as florestas, combater a desertificação, deter e reverter a degradação da terra e deter a perda de biodiversidade.
-        
+
         ODS 16. Paz, justiça e instituições eficazes:
         Objetivo 16. Promover sociedades pacíficas e inclusivas para o desenvolvimento sustentável, proporcionar o acesso à justiça para todos e construir instituições eficazes, responsáveis e inclusivas em todos os níveis.
-        
+
         ODS 17. Parcerias e meios de implementação:
         Objetivo 17. Fortalecer os meios de implementação e revitalizar a parceria global para o desenvolvimento sustentável.
-        
+
         """
-        
+
         - Responda apenas o número e nome da ODS.
         - Caso haja mais que uma, responda em formato de lista, como no exemplo:
             - ODS 10. Redução das desigualdades
             - ODS 8. Trabalho decente e crescimento econômico
-       
+
         '''
         response = self.client.models.generate_content(
-             model='models/gemini-2.0-flash',
+            model='models/gemini-2.0-flash',
             contents=[prompt, arquivo],
             config=types.GenerateContentConfig(
                 safety_settings=self.get_safety_settings(),
@@ -178,5 +152,3 @@ class GeradorAssuntoODS:
             )
         )
         return response.text
-    
-    
